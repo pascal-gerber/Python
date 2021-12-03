89105a839f4361f12120976e9ec5e5dd3a7a72d2
pascal gerber <pascal.gerber@autismuslink.ch>
2021-12-03 10:41:26 +0100

    everycommand

--- Tutorials/everycommand/mainexecutable.py
+++ Tutorials/everycommand/mainexecutable.py
@@ -6,6 +6,7 @@ import sys
 
 
 def buildpath(filename):
+    global pathtofiles
     pathtofiles = ""
     droppedFile = sys.argv[0]
     droppedFile = droppedFile.split("\\")
@@ -13,21 +14,46 @@ def buildpath(filename):
     for each in droppedFile:
         pathtofiles += each + "\\"
     pathtofiles += filename
-    subprocess.call(["C:\Python310\Lib\idlelib\idle.bat", pathtofiles])
+    openpython()
+
+def openpython():
+    global pathtofiles
+    exists = False
+    for i in reversed(range(11)):
+        try:
+            subprocess.call(["C:\Python3" + str(i) + "\Lib\idlelib\idle.bat", pathtofiles])
+            exists = True
+            break
+        except:
+            pass
+    if exists == False:
+        newWindow = Tk()
+        Label(newWindow, text="you do not have idle.\n you should try to download idle").grid(rowspan=3)
+        Button(newWindow, text="download\nidle", command=openbrowser, width=10, height=5).grid()
+        newWindow.geometry("300x300")
+        newWindow.title("install idle")
+        newWindow.mainloop()
+            
+
+def openbrowser():
+    import webbrowser
+    webbrowser.open("https://www.python.org/downloads/release/python-3100/", new=0, autoraise=True)
 
 def number(num):
-    fileNames = ["Print.py", "variables1.py", "variables2.py", "variables3.py"]
+    fileNames = ["Print.py", "variables1.py", "variables2.py", "variables3.py", "Variable changes 1.py",
+                 "List options.py", "If function.py", "Loops.py"]
     buildpath(fileNames[num])
 
 def createinterface():
     window = Tk()
-    titles = ["print explaination", "Variables part 1", "Variables part 2", "conversion of variables"]
+    titles = ["print explaination", "Variables part 1", "Variables part 2", "conversion of\nvariables", "string and lists",
+              "List sorting", "if function", "python loops"]
     mylist = Listbox(window, yscrollcommand = Scrollbar.set)
     for easystages in range(len(titles)):
         easy = Button(window, text=titles[easystages], bg="Aqua",
                       command=lambda number = number, easystages = easystages:number(easystages),
                       height = 5, width = 15)
-        easy.grid(row = int(easystages//4), column = easystages - (easystages//4))
+        easy.grid(row = int(easystages//4), column = easystages - (easystages//4)*4)
     window.geometry("500x500")
     window.mainloop()
             
